3f86e96ca552d3cf60d0ff06a86e8ee1e76906fa
Patrick Paiva <patrickpaiva88@gmail.com>
2021-03-12 10:50:20 -0300

    added tests to Delete General Entry to full coverage

--- src/modules/generalEntries/services/DeleteGeneralEntryService.spec.ts
+++ src/modules/generalEntries/services/DeleteGeneralEntryService.spec.ts
@@ -1,5 +1,6 @@
 import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository'
 import CreateUserService from '@modules/users/services/CreateUserService'
+import AppError from '@shared/errors/AppError'
 import { v4 as uuid } from 'uuid'
 import FakeGeneralEntriesRepository from '../repositories/fakes/FakeGeneralEntriesRepository'
 import CreateGeneralEntryService from './CreateGeneralEntryService'
@@ -71,5 +72,50 @@ describe('CreateGeneralEntry', () => {
 
     expect(generalEntriesList[0].deleted).toBe(true)
     expect(generalEntriesList[0].deleted_by).toBe(deleteData.userId)
+  })
+  it('should not be able to delete a General Entry with a invalid User', async () => {
+    const newGeneralEntry = {
+      date: new Date(),
+      description: 'Descrição de Teste',
+      value: 5000,
+      type: 'Tipo Teste',
+      status: 'A Pagar',
+      cost_center: 'Centro de custo de teste',
+      presentation_rubric: 'Rubrica de Apresentação de teste',
+      specific_rubric: 'Rubrica Especifica de Teste',
+      statement_id: uuid(),
+      created_by: uuid(),
+      authorized_by: uuid(),
+    }
+
+    const firstEntry = await createGeneralEntry.execute(newGeneralEntry)
+
+    const deleteData = {
+      generalEntryId: firstEntry.id,
+      userId: uuid(),
+    }
+
+    await expect(deleteGeneralEntry.execute(deleteData)).rejects.toBeInstanceOf(
+      AppError,
+    )
+  })
+  it('should not be able to delete a General Entry with invalid Id', async () => {
+    const newUser = {
+      name: 'Patrick',
+      email: 'teste@teste.com',
+      level: 2,
+      password: '123456',
+    }
+
+    const fakeUser = await createUser.execute(newUser)
+
+    const deleteData = {
+      generalEntryId: uuid(),
+      userId: fakeUser.id,
+    }
+
+    await expect(deleteGeneralEntry.execute(deleteData)).rejects.toBeInstanceOf(
+      AppError,
+    )
   })
 })
