5fc0cdb6f1d8d05d1b55a445109582caa917ea4f
Patrick Paiva <patrickpaiva88@gmail.com>
2021-03-15 15:45:20 -0300

    created DeleteStatementService

---
+++ src/modules/statements/services/DeleteStatementService.ts
@@ -0,0 +1,62 @@
+import IGeneralEntriesRepository from '@modules/generalEntries/repositories/IGeneralEntriesRepository'
+import UnlinkGeneralEntryToStatementService from '@modules/generalEntries/services/UnlinkGeneralEntryToStatementService'
+import IStatementRepository from '@modules/statements/repositories/IStatementsRepository'
+import IUsersRepository from '@modules/users/repositories/IUsersRepository'
+import AppError from '@shared/errors/AppError'
+import { inject, injectable } from 'tsyringe'
+import { validate } from 'uuid'
+// import { validate } from 'uuid'
+
+interface IRequest {
+  statementId: string
+  userId: string
+}
+
+@injectable()
+class DeleteStatementService {
+  constructor(
+    @inject('GeneralEntriesRepository')
+    private generalEntriesRepository: IGeneralEntriesRepository,
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+    @inject('StatementsRepository')
+    private statementsRepository: IStatementRepository,
+  ) {}
+
+  public async execute({ statementId, userId }: IRequest): Promise<void> {
+    const findStatement = await this.statementsRepository.findById(statementId)
+    const findUser = await this.usersRepository.findById(userId)
+
+    if (!findUser) {
+      throw new AppError('User not found', 404)
+    }
+
+    if (!findStatement) {
+      throw new AppError('Statement not found', 404)
+    }
+
+    if (findStatement.entry_id && validate(findStatement.entry_id)) {
+      const unlinkGeneralEntryToStatementService = new UnlinkGeneralEntryToStatementService(
+        this.generalEntriesRepository,
+        this.statementsRepository,
+      )
+
+      await unlinkGeneralEntryToStatementService.execute({
+        id: findStatement.entry_id,
+        statement_id: findStatement.id,
+      })
+    }
+
+    const deletedStatement = {
+      ...findStatement,
+      entry_id: null,
+      deleted: true,
+      deleted_by: userId,
+      deleted_date: new Date(),
+    }
+
+    await this.statementsRepository.update(deletedStatement)
+  }
+}
+
+export default DeleteStatementService
